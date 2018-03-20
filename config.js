f40b4b645c6a8e5e76359d0391bb57b15f188912
Ali Saeed <asalpha01@gmail.com>
2018-03-19 21:31:26 -0400

    resume upload

---
+++ config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":1,"title":"","author":"Ali Saeed","fileName":"Resume.pdf","bounds":[[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
